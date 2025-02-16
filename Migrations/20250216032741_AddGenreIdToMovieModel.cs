409316a34894a7088c70f25f56caf41dd314891f
Manuel Villagrán <manuel.7000@hotmail.com>
2025-02-16 00:28:36 -0300

    Add GenreId to Movie model

---
+++ Migrations/20250216032741_AddGenreIdToMovieModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieReservation.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenreIdToMovieModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genres_GenreId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Movies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Genres_GenreId",
+                table: "Movies",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Genres_GenreId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Movies",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Genres_GenreId",
+                table: "Movies",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id");
+        }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,6 +7,7 @@ public class Movie
 	public int Id { get; set; }
 	public string Title { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
+	public int GenreId { get; set; }
 	public Genre? Genre { get; set; }
 	public string PosterUrl = string.Empty;
 	public int DurationInMinutes {get; set;}
